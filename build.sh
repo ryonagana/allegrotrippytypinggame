af77bf07263e1341f6b7223e0e2c458c8f2c8ea8
ryonagana <nicholasluis@gmail.com>
2023-02-15 08:47:46 -0300

    fixed build  script

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash 
 #this script is for RELEASE only
 
-make -f Makefile.linux_release
+params="$1"
 
+
+case $params in
+
+	build)
+	make -f Makefile.linux_release
+	;;
+
+	build-assets)
+	make -f Makefile.linux_release create_zip
+	;;
+
+	*)
+	make -f Makefile.linux_release
+	;;
+esac
